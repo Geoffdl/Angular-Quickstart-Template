02895c3333d9da7a58a97653c7c94f5b3360d7c5
Geoffdl <geoffroydelorme@gmail.com>
2025-06-29 11:03:51 +0200

    More default routes and import optimisation

---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,6 @@
+export * from './github.component';
+export * from './hamburger-menu.component';
+export * from './icon-moon.component';
+export * from './icon-sun.component';
+export * from './linkedin.component';
+export * from './user-icon.component';
